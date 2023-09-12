f13860a75d219c07bd151e3a31dc8157b11d347a
Raihan <raihanuzzaman@bs-23.net>
2023-09-12 20:45:14 +0600

    added service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ScreenSizeService} from "./common-service/screen-size.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  screenSize: string = "";
   title = 'ang_exp';
+
+  constructor(private screenSizeService: ScreenSizeService) {}
+
+  ngOnInit(): void {
+    this.screenSizeService.getScreenSize$().subscribe(size => {
+      this.screenSize = size;
+    });
+  }
 }
