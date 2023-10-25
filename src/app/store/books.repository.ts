1f5689d3e6cb4f812ac853804ab71f57b1e24412
Raihan <raihanuzzaman@bs-23.net>
2023-10-25 20:42:30 +0600

    added repository and local db

--- src/app/store/books.repository.ts
+++ src/app/store/books.repository.ts
@@ -1,20 +1,30 @@
 import {
   selectAllEntities,
+  selectEntities,
   setEntities,
   withEntities,
 } from '@ngneat/elf-entities';
-import { createState, Store } from '@ngneat/elf';
+import { createState, propsArrayFactory, Store } from '@ngneat/elf';
 import { Injectable } from '@angular/core';
+import { map, withLatestFrom } from 'rxjs';
 
 export interface Book {
-  id: number;
+  id: string;
   valumeInfo: {
     title: string;
     authors: string[];
   };
 }
 
-const { state, config } = createState(withEntities<Book>());
+const {
+  withLibraryIds,
+  selectLibraryIds,
+  addLibraryIds,
+  removeLibraryIds,
+  inLibraryIds,
+} = propsArrayFactory('libraryIds', { initialValue: [] as string[] });
+
+const { state, config } = createState(withEntities<Book>(), withLibraryIds());
 
 const store = new Store({ name: 'books', state, config });
 
@@ -22,7 +32,22 @@ const store = new Store({ name: 'books', state, config });
 export class BooksRepository {
   books$ = store.pipe(selectAllEntities());
 
+  ownBooks$ = store.pipe(selectLibraryIds()).pipe(
+    withLatestFrom(store.pipe(selectEntities())),
+    map(([ids, books]) => ids.map(id => books[id]))
+  );
+
   setBooks(books: Book[]) {
     store.update(setEntities(books));
+  }
+
+  removeFromCollection(bookId: string) {
+    store.update(removeLibraryIds(bookId));
+  }
+
+  addToCollection(bookId: string) {
+    if (!store.query(inLibraryIds(bookId))) {
+      store.update(addLibraryIds(bookId));
+    }
   }
 }
