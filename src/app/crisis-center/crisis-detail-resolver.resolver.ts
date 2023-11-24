46acc9327e85feb8862bbb693761a39bca127822
Raihan <raihanuzzaman@bs-23.net>
2023-11-24 20:54:10 +0600

    commit test 5 + 5

--- src/app/crisis-center/crisis-detail-resolver.resolver.ts
+++ src/app/crisis-center/crisis-detail-resolver.resolver.ts
@@ -19,6 +19,7 @@ export const crisisDetailResolverResolver: ResolveFn<Crisis> = route => {
         console.log(3 + 3);
         console.log(2 + 2);
         console.log(4 + 4);
+        console.log(5 + 5);
         router.navigate(['/crisis-center']);
         return EMPTY;
       }
